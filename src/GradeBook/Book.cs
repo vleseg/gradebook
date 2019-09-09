d9af84b97dabd0076e093740647122eedc6182b8
Vladimir Sirota <vlesiil@yandex.ru>
2019-09-09 12:12:12 +0700

    First commit

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.High = Math.Max(result.High, grade);
+                result.Low = Math.Min(result.Low, grade);
+                result.Average += grade;
+            }
+            result.Average /= grades.Count;
+
+            return result;
+        }
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Vladimir's Gradebook");
+            book.AddGrade(49.5);
+            book.AddGrade(90.1);
+            book.AddGrade(6.1);
+            book.AddGrade(5.0);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+        }
+    }
+}
